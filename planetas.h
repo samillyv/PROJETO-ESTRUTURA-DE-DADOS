6dc9e6a695fdf4efba972a2a0d3e2eae425d7043
samilly <samilleria@gmail.com>
2026-05-12 12:43:30 -0300

    primeiros arquivos

---
+++ planetas.h
@@ -0,0 +1,31 @@
+#ifndef PLANETAS_H
+#define PLANETAS_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Planeta {
+    string nome;
+    int ordemSolar;
+    float x;
+    float y;
+    string tipo;
+    float sobrevivencia;
+};
+
+struct No {
+    Planeta planeta;
+    No* esquerda;
+    No* direita;
+};
+
+No* criarNo( Planeta p);
+
+No* inserirPlaneta(No* raiz, Planeta p);
+
+void mostrarPlanetas(No* raiz);
+
+No* buscarPlaneta(No* raiz, string nome);
+
+#endif
