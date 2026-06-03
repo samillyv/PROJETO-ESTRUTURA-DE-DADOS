8dc9c866f1f875e67e50ebd6fff53576859766a1
DennisJose-052 <dennisjose052@gmail.com>
2026-06-03 04:06:47 -0300

    Add naves.h header file with structures and prototypes

--- naves.h
+++ naves.h
@@ -1,1 +1,35 @@
+#ifndef NAVES_H
+#define NAVES_H
 
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+// Dados da nave
+struct Nave {
+    string nome;
+    int capacidade;
+    float velocidade;
+};
+
+// Dados da viagem
+struct Viagem {
+    Nave nave;
+    string origem;
+    string destino;
+    float distancia;
+};
+
+// Fila principal
+extern queue<Viagem> filaViagens;
+
+// Protótipos
+void adicionarViagem();
+void processarViagem();
+void mostrarFila();
+void mostrarPrimeiraViagem();
+
+void menuNaves();
+
+#endif
