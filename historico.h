c985745c2dbc9f0dfca60edf79e3c5f965f9124b
DennisJose-052 <dennisjose052@gmail.com>
2026-06-03 04:05:10 -0300

    Define NoHistorico struct and travel history functions
    Added structure and function declarations for managing travel history.

--- historico.h
+++ historico.h
@@ -1,1 +1,23 @@
+#ifndef HISTORICO_H
+#define HISTORICO_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Nó da lista duplamente encadeada
+struct NoHistorico {
+    string destino;
+    NoHistorico* anterior;
+    NoHistorico* proximo;
+};
+
+// Funções
+void registrarViagemHistorico(string destino);
+void mostrarHistorico();
+void voltarHistorico();
+
+void menuHistorico();
+
+#endif
