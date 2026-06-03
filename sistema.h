cca0cf56f2eb245b1d3a32cb77225d125caf96cc
DennisJose-052 <dennisjose052@gmail.com>
2026-06-03 04:09:15 -0300

    Add function declarations for planet distance calculations

--- sistema.h
+++ sistema.h
@@ -1,1 +1,34 @@
+#ifndef SISTEMA_H
+#define SISTEMA_H
 
+#include "planetas.h"
+
+float calcularDistanciaPlanetas(
+    Planeta p1,
+    Planeta p2
+);
+
+void mostrarDistanciaEntrePlanetas(
+    NoPlaneta* raiz,
+    string nome1,
+    string nome2
+);
+
+void calcularDistanciaMenu();
+
+void coletarPlanetas(
+    NoPlaneta* raiz,
+    Planeta vetor[],
+    int& quantidade
+);
+
+void ordenarSobrevivencia(
+    Planeta vetor[],
+    int quantidade
+);
+
+void mostrarRankingSobrevivencia();
+
+void menuSistema();
+
+#endif
